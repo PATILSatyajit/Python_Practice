446e0ee80ccec676c2631a86fd3e7fe9016a88f8
PATILSatyajit <16510317.dypit@dypvp.edu.in>
2021-09-17 21:12:22 +0530

    practice

---
+++ 1.Basic_input_output_data_types_typecast.py
@@ -0,0 +1,47 @@
+# print("Hello World")
+'''
+1.';' not used
+2. print <--> printf
+'''
+'''
+Data Types
+1. integer
+2. float
+3. String - 'satyajit'
+4. Boolean - True , False 
+'''
+
+integer_i = 5
+float_f = 4.2
+character_c = 'a'
+string_s = 'pratiksha'
+boolean_b = True
+
+# print("Satyajit \nPatil")
+
+'''f-string'''
+# print(f"{integer_i}\n{float_f}\n{string_s}")
+
+# print("type of",integer_i,"is",type(integer_i))
+# print("type of",float_f,"is",type(float_f))
+# print("type of",character_c,"is",type(character_c))
+# print("type of",string_s,"is",type(string_s))
+# print("type of",boolean_b,"is",type(boolean_b))
+
+'''
+user input
+program 1 - addition of two user defined numbers a & b
+hint - scanf() <--> input()
+'''
+# print("Enter two numbers")
+
+''' typecasting '''
+# a = int(input())
+# b = float(input())
+
+# print(type(a))
+# print(type(b))
+
+# c = a + b
+# print("sum of a & b is",a + b)
+# print(f"sum of {a} and {b} is {a+b}")
---
+++ 2.Arithmetic_operations.py
@@ -0,0 +1,20 @@
+'''
+addition 9+4=13
+subtraction 9-4=5
+multiplication 9*4=36
+division 9/4=2.25   
+mudulus 9%4 = 1 (quotient --> baaki)
+floor division 9//4 = 2
+power 9**4
+'''
+
+a = int(input("enter value of a: "))
+b = int(input("enter value of b: "))
+
+print(f"sum of {a} and {b} is {a+b}")
+print(f"subtraction of {a} and {b} is {a-b}")
+print(f"division of {a} and {b} is {a/b}")
+print(f"multiplication of {a} and {b} is {a*b}")
+print(f"floor division of {a} and {b} is {a//b}")
+print(f"modulus of {a} and {b} is {a%b}")
+print(f"{a}raised to {b}is{9**4}")
